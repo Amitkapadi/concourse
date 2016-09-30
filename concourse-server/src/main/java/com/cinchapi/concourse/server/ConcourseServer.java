60d243603d1afc10b43e0ea8a5b68ba5acb7fab9
RaghavBabu <raghavbabu11@gmail.com>
2016-09-30 13:55:34 -0400

    Changed the method parameter type to Object for setPreference

--- concourse-server/src/main/java/com/cinchapi/concourse/server/ConcourseServer.java
+++ concourse-server/src/main/java/com/cinchapi/concourse/server/ConcourseServer.java
@@ -4479,7 +4479,7 @@ public class ConcourseServer
     }
 
     @Override
-    public void setPreference(String key, String value) {
+    public void setPreference(String key, Object value) {
         ConcourseServerPreferences.open(GlobalState.getPrefsFilePath())
                 .setProperty(key, value);
     }
